4549d47028a21d22f1d117fba6b456016ea718f8
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-03 00:30:42 +0800

    Moved setTimout and Promise on top. Called a second method after foo().

--- nodejs-event-loop.js
+++ nodejs-event-loop.js
@@ -4,13 +4,17 @@ const bar = () => console.log('bar');
 
 const baz = () => console.log('baz');
 
+const hani = () => console.log('hani');
+
 const foo = () => {
-    console.log('foo');
+    // Moving setTimeout and Promise at the start would still put them after the function.
     setTimeout(bar, 0);
     new Promise((resolve, reject) => {
         resolve('should be right after baz, before bar')
     }).then(resolve => console.log(resolve));
+    console.log('foo');
     baz();
 }
 
 foo();
+hani(); // This executes even before the Promise of foo().
