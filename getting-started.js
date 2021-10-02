839da731d72176b9ae4d6cee07bbc1e354301fc1
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-02 09:01:47 +0800

    Removed code for web.

--- getting-started.js
+++ getting-started.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-const express = require('express');
-const { Server } = require('http');
-const app = express();
-
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readline.question(`What's your age?`, age => {
+readline.question(`What's your age? `, age => {
     console.log(`You're ${age} years old.`);
     readline.close();
 })
-
-app.get('/', (req, res) => {
-    res.send('Hi!');
-});
-
-app.listen(3000, () => console.log('Server read'));
