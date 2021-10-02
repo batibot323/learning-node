5d82cc53ffea13005cbdb6b2aa9e354c06ebd270
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-03 00:36:24 +0800

    Duplicated the method to further understand.

--- nodejs-event-loop.js
+++ nodejs-event-loop.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 
 const bar = () => console.log('bar');
+const barHani = () => console.log(`This is hani's bar.`);
 
 const baz = () => console.log('baz');
 
-const hani = () => console.log('hani');
+// Adding another method with a whole set of setTimeout(), Promise, and nextTick()
+const hani = () =>  {
+    process.nextTick(() => {
+        console.log('Use of nextTick() - hani');
+    })
+    setTimeout(barHani, 0);
+    new Promise((resolve, reject) => {
+        resolve('should be right after baz, before bar - hani')
+    }).then(resolve => console.log(resolve));
+    console.log('hani');
+}
 
 const foo = () => {
     // Moving setTimeout and Promise at the start would still put them after the function.
+    process.nextTick(() => {
+        console.log('Use of nextTick()');
+    })
     setTimeout(bar, 0);
     new Promise((resolve, reject) => {
         resolve('should be right after baz, before bar')
