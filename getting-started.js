ce08e40ebbcbaedb8dad9c43fdcebaa2fd5d73a5
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-02 08:14:29 +0800

    Doing 'How to exit'.

---
+++ getting-started.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi!')
+});
+
+app.listen(3000, () => console.log('Server read'));
