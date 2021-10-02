03591ce12cc0624a04de7e40fe27c152d114dd27
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-02 08:28:18 +0800

    Reading env variables.

--- getting-started.js
+++ getting-started.js
@@ -4,15 +4,19 @@ const express = require('express');
 const { Server } = require('http');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hi!');
-    process.kill(process.pid, 'SIGTERM');
-});
+require('dotenv').config();
 
-app.listen(3000, () => console.log('Server read'));
+app.get('/', (req, res) => {
+    console.log(process.env.USER_ID);
+    console.log(process.env.USER_KEY);
+    console.log(process.env.NODE_ENV);
+    res.send('Hi!');
+});
 
 process.on('SIGTERM', () => {
     Server.close(() => {
         console.log('Process terminated');
     });
 });
+
+app.listen(3000, () => console.log('Server read'));
