94d971a5c1322ae99a86fa459318436f8dfba50e
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-02 08:21:46 +0800

    Now ending the program after the URL being reached once.

--- getting-started.js
+++ getting-started.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
 const express = require('express');
+const { Server } = require('http');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi!')
+    res.send('Hi!');
+    process.kill(process.pid, 'SIGTERM');
 });
 
 app.listen(3000, () => console.log('Server read'));
+
+process.on('SIGTERM', () => {
+    Server.close(() => {
+        console.log('Process terminated');
+    });
+});
