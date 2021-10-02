8aef424564e94903132065331a644bd5285c5264
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-03 01:13:06 +0800

    Intro to JS Promises.

---
+++ learn-promises.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+let done = true;
+
+const isItDoneYet = new Promise((resolve, reject) => {
+    if (done) {
+        const workDone = 'Here is the thing I built';
+        resolve(workDone);
+    } else {
+        const why = 'Still working on something else';
+        reject(why);
+    }
+});
+
+const checkIfItsDone = () => {
+    isItDoneYet
+        .then(ok => {
+            console.log(ok);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+}
+
+checkIfItsDone();
