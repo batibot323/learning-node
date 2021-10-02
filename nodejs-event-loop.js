4adab0804c0d5d29fef744188029a6d1a61ee61f
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-03 00:46:05 +0800

    Added setImmediate().

--- nodejs-event-loop.js
+++ nodejs-event-loop.js
@@ -7,6 +7,9 @@ const baz = () => console.log('baz');
 
 // Adding another method with a whole set of setTimeout(), Promise, and nextTick()
 const hani = () =>  {
+    setImmediate(() => {
+        console.log('setImmediate - hani');
+    })
     process.nextTick(() => {
         console.log('Use of nextTick() - hani');
     })
@@ -19,6 +22,9 @@ const hani = () =>  {
 
 const foo = () => {
     // Moving setTimeout and Promise at the start would still put them after the function.
+    setImmediate(() => {
+        console.log('setImmediate');
+    })
     process.nextTick(() => {
         console.log('Use of nextTick()');
     })
@@ -32,3 +38,18 @@ const foo = () => {
 
 foo();
 hani(); // This executes even before the Promise of foo().
+
+// The two lines above prints this:
+/*
+foo
+baz
+hani
+Use of nextTick()
+Use of nextTick() - hani
+should be right after baz, before bar
+should be right after baz, before bar - hani
+bar
+This is hani's bar.
+setImmediate
+setImmediate - hani
+*/
