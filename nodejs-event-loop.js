9560f4e08b7db7502fea621aced2f6515884c6c4
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-03 00:17:29 +0800

    Added use of Promise.

--- nodejs-event-loop.js
+++ nodejs-event-loop.js
@@ -7,6 +7,9 @@ const baz = () => console.log('baz');
 const foo = () => {
     console.log('foo');
     setTimeout(bar, 0);
+    new Promise((resolve, reject) => {
+        resolve('should be right after baz, before bar')
+    }).then(resolve => console.log(resolve));
     baz();
 }
 
