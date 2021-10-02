8346a12721b79f3bdf3457a80a0b3ef6f53818a3
Hani Ho <hani.herbert.ho@gmail.com>
2021-10-02 08:49:30 +0800

    Accept arguments from CLI.

--- getting-started.js
+++ getting-started.js
@@ -4,19 +4,13 @@ const express = require('express');
 const { Server } = require('http');
 const app = express();
 
-require('dotenv').config();
-
 app.get('/', (req, res) => {
-    console.log(process.env.USER_ID);
-    console.log(process.env.USER_KEY);
-    console.log(process.env.NODE_ENV);
-    res.send('Hi!');
-});
-
-process.on('SIGTERM', () => {
-    Server.close(() => {
-        console.log('Process terminated');
+    // Run with
+    // node ./getting-started.js Hani
+    process.argv.forEach((val, index) => {
+        console.log(`${index}: ${val}`)
     });
+    res.send('Hi!');
 });
 
 app.listen(3000, () => console.log('Server read'));
